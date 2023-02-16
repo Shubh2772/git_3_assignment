dbf2e6082aeef700aeed7b50a1934c952371950a
unknown <shubhamsaini2772@gmail.com>
2023-02-16 17:43:17 +0530

    using ternary

--- prime.js
+++ prime.js
@@ -1,11 +1,8 @@
 let num=15;
 let count=0;
-for(let i=0; i<num; i++)
-{if(num%2===0){
+for(let i=1; i<=num; i++)
+{if(num%2==0){
 count++;
 }
 }
-if(count==1){
-console.log("it's a prime");
-}else{
-console.log("it's not a prime");}
+(count==2) ? (console.log("it's a prime")):(console.log("it's not a prime"))
